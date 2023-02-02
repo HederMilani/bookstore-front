acdbbde64c1b2c2e0decd71a093482c1462c4ad5
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2023-02-02 19:02:57 -0300

    ADD - Adicionar View para mostrar as categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './components/view/home/home.component';
+import { CategoriaReadComponent } from './components/view/categoria/categoria-read/categoria-read.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { HomeComponent } from './components/view/home/home.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriaReadComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +35,8 @@ import { HomeComponent } from './components/view/home/home.component';
 	MatSidenavModule,
 	MatListModule,
 	MatIconModule,
-	MatCardModule
+	MatCardModule,
+	MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
